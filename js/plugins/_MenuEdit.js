38d6da1ac5535039cb73581859b077dab691a0b1
Antamansid <Antamansid@mail.ru>
2020-02-22 23:57:02 +0300

    Откомментил код. Вынес проверку на Главное меню выше.

--- js/plugins/_MenuEdit.js
+++ js/plugins/_MenuEdit.js
@@ -10,22 +10,34 @@
  */
 
 (function() {
-  var _Window_Selectable_prototype_processCursorMove = Window_Selectable.prototype.processCursorMove;
-  Window_Selectable.prototype.processCursorMove = function() {
-      _Window_Selectable_prototype_processCursorMove.call(this);
-      if (this.isCursorMovable()) {
-          if (Input.isRepeated('down')) {
-              if(SceneManager._scene.constructor.name === "Scene_Title"){
-                  SceneManager._scene.addChildAt(new Sprite(ImageManager.loadTitle1("Book")), 0);
-                  SceneManager._scene.removeChildAt (1);
-              }
-          }
-          if (Input.isRepeated('up')) {
-              if(SceneManager._scene.constructor.name === "Scene_Title"){
-                  SceneManager._scene.addChildAt(new Sprite(ImageManager.loadTitle1("Castle")), 0);
-                  SceneManager._scene.removeChildAt (1);
-              }
-          }
-      }
-  };
-})();
+    //Сначала сделаем копию на изначальный объект, чтобы перед редактированием запустить стандартный код
+    var _Window_Selectable_prototype_processCursorMove = Window_Selectable.prototype.processCursorMove;
+    //Теперь редактируем стандартный код
+    Window_Selectable.prototype.processCursorMove = function() {
+      //Запускаем стандартный скопированный код (смотри строчку 15)
+        _Window_Selectable_prototype_processCursorMove.call(this);
+        //нам нужно, чтобы обработка событий происходила только в сцене меню. Это Scene_Title
+        if(SceneManager._scene.constructor.name === "Scene_Title"){
+        //Обрабатываем событие "мышь двигалась"
+            if (this.isCursorMovable()) {
+            //Проверяем какие нажаты клавиши
+            //Если нажата клавиша вниз
+                if (Input.isRepeated('down')) {
+                //Добавляем спрайт изображения на нулевую позицию
+                SceneManager._scene.addChildAt(new Sprite(ImageManager.loadTitle1("Book")), 0);
+                //Когда мы добавляем новый спрайт на нулевую позицию, старый нулевой спрайт становится 1ым
+                //Удаляем его
+                SceneManager._scene.removeChildAt (1);
+                }
+                //Если нажата клавиша вверх
+                if (Input.isRepeated('up')) {
+                //Добавляем спрайт изображения на нулевую позицию
+                SceneManager._scene.addChildAt(new Sprite(ImageManager.loadTitle1("Castle")), 0);
+                //Когда мы добавляем новый спрайт на нулевую позицию, старый нулевой спрайт становится 1ым
+                //Удаляем его
+                SceneManager._scene.removeChildAt (1);
+                }
+            }
+        }
+    };
+  })();
